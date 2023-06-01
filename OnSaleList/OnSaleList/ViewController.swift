4b5a8f056e88f109d6ae1e0f60b96b2b083053b3
madversi <helderadversi@gmail.com>
2023-06-01 15:12:17 -0400

    Creates project

---
+++ OnSaleList/OnSaleList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnSaleList
+//
+//  Created by Helder Marcelo Adversi Junior on 01/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
