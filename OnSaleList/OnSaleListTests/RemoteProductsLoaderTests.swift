15e08c8fac3ed483c65232dc9911f2b978442cba
madversi <helderadversi@gmail.com>
2023-06-01 15:44:20 -0400

    Requests data from a URL by HTTPClient

--- OnSaleList/OnSaleListTests/RemoteProductsLoaderTests.swift
+++ OnSaleList/OnSaleListTests/RemoteProductsLoaderTests.swift
@@ -8,20 +8,34 @@
 import XCTest
 
 class RemoteProductsLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://any-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteProductsLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        let sut = RemoteProductsLoader()
+        let client = HTTPClient.shared
+        _ = RemoteProductsLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteProductsLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
