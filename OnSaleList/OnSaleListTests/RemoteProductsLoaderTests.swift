e23ca3a98e7780e3e71251b16342cfa539ea5b7d
madversi <helderadversi@gmail.com>
2023-06-01 15:51:09 -0400

    Moves test logic to a spy

--- OnSaleList/OnSaleListTests/RemoteProductsLoaderTests.swift
+++ OnSaleList/OnSaleListTests/RemoteProductsLoaderTests.swift
@@ -9,29 +9,41 @@ import XCTest
 
 class RemoteProductsLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "http://any-url.com")
+        HTTPClient.shared.get(from: URL(string: "http://any-url.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
     
-    private init() {}
+    func get(from url: URL) {
+        
+    }
     
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteProductsLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        
         _ = RemoteProductsLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteProductsLoader()
         
         sut.load()
