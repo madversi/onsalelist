40239429efacb617f09e17213af0f361541f5cc9
madversi <helderadversi@gmail.com>
2023-06-01 16:37:31 -0400

    Adds test to assert load func call count

--- OnSaleList/OnSaleListTests/RemoteProductsLoaderTests.swift
+++ OnSaleList/OnSaleListTests/RemoteProductsLoaderTests.swift
@@ -25,6 +25,16 @@ class RemoteProductsLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "http://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "http://a-given-url.com")!) -> (sut: RemoteProductsLoader, client: HTTPClientSpy) {
@@ -35,9 +45,11 @@ class RemoteProductsLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
