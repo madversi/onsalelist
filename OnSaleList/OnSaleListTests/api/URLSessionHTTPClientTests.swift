75b7af8f2f92b472645acfba4357ec8ae711f873
madversi <helderadversi@gmail.com>
2023-06-06 22:58:53 -0400

    Adds test to cover unexpected error

--- OnSaleList/OnSaleListTests/api/URLSessionHTTPClientTests.swift
+++ OnSaleList/OnSaleListTests/api/URLSessionHTTPClientTests.swift
@@ -15,10 +15,14 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
+    struct UnexpectedError: Error { }
+    
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedError()))
             }
         }.resume()
     }
@@ -61,6 +65,24 @@ class URLSessionHTTPClientTests: XCTestCase {
                 XCTAssertEqual(receivedError.code, error.code)
             default:
                 XCTFail("Expected failure \(error), got \(result).")
+            }
+            getExpectation.fulfill()
+        }
+        
+        wait(for: [getExpectation], timeout: 1.0)
+    }
+    
+    func test_getFromURL_failsOnNilValues() {
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+        
+        let getExpectation = expectation(description: "Wait for get to finish")
+
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
+            default:
+                XCTFail("Expected failure, got \(result).")
             }
             getExpectation.fulfill()
         }
