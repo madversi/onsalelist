615a9a54aeeeaf4c246732033f51578eb183c8f2
madversi <helderadversi@gmail.com>
2023-06-07 00:11:29 -0400

    Moves `URLSessionHTTPClient` to a new file

---
+++ OnSaleList/OnSaleList/api/URLSessionHTTPClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHTTPClient.swift
+//  OnSaleList
+//
+//  Created by Helder Marcelo Adversi Junior on 07/06/23.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedError: Error { }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedError()))
+            }
+        }.resume()
+    }
+}
--- OnSaleList/OnSaleListTests/api/URLSessionHTTPClientTests.swift
+++ OnSaleList/OnSaleListTests/api/URLSessionHTTPClientTests.swift
@@ -8,28 +8,6 @@
 import OnSaleList
 import XCTest
 
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedError: Error { }
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedError()))
-            }
-        }.resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
     
     override func setUp() {
