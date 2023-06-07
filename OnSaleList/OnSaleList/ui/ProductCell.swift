fbbd37726b6184bf9216ca08b6c432ba1d03127d
madversi <helderadversi@gmail.com>
2023-06-07 11:57:04 -0400

    Creates outlets and `ProductCellViewModel` in `ProductCell`

--- OnSaleList/OnSaleList/ui/ProductCell.swift
+++ OnSaleList/OnSaleList/ui/ProductCell.swift
@@ -10,9 +10,35 @@ import UIKit
 class ProductCell: UITableViewCell {
     
     // MARK: - Outlets
-    
-    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var productOnSaleLabel: UILabel!
+    @IBOutlet weak var productSalePriceLabel: UILabel!
+    @IBOutlet weak var productAvailableSizesLabel: UILabel!
+    @IBOutlet weak var addToChartButton: UIButton!
     
     // MARK: - Setup
+    func configureProductCell(with productCellViewModel: ProductCellViewModel) {
+        
+    }
     
+}
+
+class ProductCellViewModel {
+    let image: UIImage
+    let name: String
+    let price: String
+    let onSale: String
+    let salePrice: String
+    let availableSizes: String
+    
+    init(image: UIImage, name: String, price: String, onSale: String, salePrice: String, availableSizes: String) {
+        self.image = image
+        self.name = name
+        self.price = price
+        self.onSale = onSale
+        self.salePrice = salePrice
+        self.availableSizes = availableSizes
+    }
 }
