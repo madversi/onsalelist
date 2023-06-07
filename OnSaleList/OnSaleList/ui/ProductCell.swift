0beb35fdbc40fd1f49e24266497a95d4b4e4fad2
madversi <helderadversi@gmail.com>
2023-06-07 11:43:17 -0400

    Creates `ProductCellView`

---
+++ OnSaleList/OnSaleList/ui/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  OnSaleList
+//
+//  Created by Helder Marcelo Adversi Junior on 07/06/23.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+    
+    // MARK: - Outlets
+    
+    
+    
+    // MARK: - Setup
+    
+}
