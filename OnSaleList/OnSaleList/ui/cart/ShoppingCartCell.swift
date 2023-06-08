3b0e65dc09ab4ebc2e37a3d602221b9bb98031bd
madversi <helderadversi@gmail.com>
2023-06-08 18:15:52 -0400

    Adds `ShoppingCartCellView` outlets into its view controller

--- OnSaleList/OnSaleList/ui/cart/ShoppingCartCell.swift
+++ OnSaleList/OnSaleList/ui/cart/ShoppingCartCell.swift
@@ -9,4 +9,10 @@ import UIKit
 
 class ShoppingCartCell: UITableViewCell {
     
+    // MARK: - Outlets
+    @IBOutlet weak var shoppingCartImageView: UIImageView!
+    @IBOutlet weak var shoppingCartNameLabel: UILabel!
+    @IBOutlet weak var shoppingCartPriceLabel: UILabel!
+    @IBOutlet weak var shoppingCartQuantityLabelData: UILabel!
+    @IBOutlet weak var shoppingCartMinusButton: UIButton!
 }
