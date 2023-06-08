f377d2b9689f61c973d0b73232684b840fedc3e1
madversi <helderadversi@gmail.com>
2023-06-08 18:10:49 -0400

    Creates `ShoppingCartCell` and `ShoppingCartViewController`

---
+++ OnSaleList/OnSaleList/ui/cart/ShoppingCartCell.swift
@@ -0,0 +1,12 @@
+//
+//  ShoppingCartCell.swift
+//  OnSaleList
+//
+//  Created by Helder Marcelo Adversi Junior on 08/06/23.
+//
+
+import UIKit
+
+class ShoppingCartCell: UITableViewCell {
+    
+}
